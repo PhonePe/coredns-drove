276fcdd1d94f7fcc9fedb03d313e319b8618b782
Shubham Sharma <shubham.sharma@phonepe.com>
2024-08-13 21:49:39 +0530

    Add Docker support

--- cmd/coredns/main.go
+++ cmd/coredns/main.go
@@ -4,10 +4,10 @@ package main
 //go:generate go run owners_generate.go
 
 import (
+	_ "github.com/PhonePe/coredns-drove"
 	"github.com/coredns/coredns/core/dnsserver"
 	_ "github.com/coredns/coredns/core/plugin" // Plug in CoreDNS.
 	"github.com/coredns/coredns/coremain"
-	_ "phonepe.com/drovedns"
 )
 
 var directives = []string{
@@ -16,6 +16,7 @@ var directives = []string{
 	"forward",
 	"ready",
 	"whoami",
+	"prometheus",
 	"cache",
 	"startup",
 	"shutdown",
